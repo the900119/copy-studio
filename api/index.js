3f250b99ffaed453a36bf043f30c447fb68677bc
the900119 <the900119@gmail.com>
2026-05-03 19:55:20 +0800

    fix: serve index.html via function to bypass HKG1 CDN cache

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function handler(req, res) {
+  const html = fs.readFileSync(path.join(process.cwd(), 'index.html'), 'utf8');
+  res.setHeader('Cache-Control', 'no-store, max-age=0');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.status(200).send(html);
+};
